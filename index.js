c7b5b60a285d45209846bb7009f2c5f67ef19c66
Mostafiz <mostafiz@iMac-Mostafizur.lan>
2022-11-24 18:18:32 +0000

    initial server set up done

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+
+const app = express();
+
+/* middleware */
+app.use(cors());
+app.use(express.json());
+
+
+app.get('/', async (req, res) => {
+    res.send('Server is running...........')
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`)
+})
