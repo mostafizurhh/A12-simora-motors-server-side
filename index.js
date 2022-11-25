1c206c0c0d0583ce139b034d84b6cdb6b54cb4c0
Mostafiz <mostafiz@iMac-Mostafizur.lan>
2022-11-25 03:44:38 +0000

    api created for all categories and all products

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 
 const app = express();
@@ -17,3 +18,40 @@ app.get('/', async (req, res) => {
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`)
 })
+
+
+/* mongodb connection */
+require('dotenv').config()
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.mniec4l.mongodb.net/?retryWrites=true&w=majority`
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+/* mongodb CRUD operation */
+async function run() {
+    try {
+        /*---------------CtaegoriesCollection-----------*/
+
+        const categoriesCollection = client.db('simora-motors').collection('product-categories');
+
+        /* (READ) get all product categories */
+        app.get('/allcategories', async (req, res) => {
+            const query = {};
+            const result = await categoriesCollection.find(query).toArray();
+            res.send(result);
+        });
+
+        /*---------------productsCollection-----------*/
+
+        const productsCollection = client.db('simora-motors').collection('all-products');
+
+        /* (READ) get all product data */
+        app.get('/allproducts', async (req, res) => {
+            const query = {};
+            const result = await productsCollection.find(query).toArray();
+            res.send(result);
+        });
+    }
+    finally {
+
+    }
+}
+run().catch(console.log)
